1095aaf61b762bc90a9d9b574817368b8e9dc19e
122333211121 <75063381+122333211121@users.noreply.github.com>
2021-07-08 15:01:24 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -4,17 +4,20 @@ import time
 from bs4 import BeautifulSoup as BS
 
 SONG_LIST = []
+# 建立储存文件夹
 file_path = './music'
 if not os.path.exists(file_path):
     os.makedirs(file_path)
 
 
 def get_the_page():
+    """
+    将所需要爬取的网页的音乐id和音乐名获取出来，然后存入列表，随时取用
+    """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
     }
-
     url = 'https://music.163.com/discover/toplist?id=3778678'
     response = requests.get(url, headers=headers).content
     s = BS(response, 'lxml')
@@ -28,9 +31,13 @@ def get_the_page():
 
 
 def get_the_music():
+    """
+    下载音乐
+    """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
     }
+    # 不同音乐的基础url一样，不同的是id，所以将其余部分设置为basic_url
     basic_url = 'http://music.163.com/song/media/outer/url?id={}.mp3'
     for song_name, song_id in SONG_LIST:
         download_url = basic_url.format(song_id)
@@ -43,7 +50,7 @@ def get_the_music():
                 with open(music_path, 'wb')as f:
                     f.write(song.content)
                     print('{}下载成功'.format(song_name))
-                    time.sleep(0.5)
+                    time.sleep(0.5)# 需要停一下，不然会被检测，然后强制拒绝连接
 
             else:
                 print('{}已经下载'.format(song_name))
